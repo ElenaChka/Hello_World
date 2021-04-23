7b2deb1983a8af3bb9c292aba5d345d3220aa08a
ElenaChka <checkalina.lena@bk.ru>
2021-04-23 22:02:30 +0300

    Hello Sasha

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Программа
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout << " CCCCC    CC   C   C    CC"<<  endl<<
+            " C       C  C  C C C   C  C"<< endl<<
+            " C      CCCCC  C C C  CCCCC"<< endl<<
+            " C      C   C  C C C  C   C"<< endl<<
+            " CCCCC  C   C  CCCCC  C   C"<< endl;
+}
